402a45bd6d608ec62fe9a44f1fba760cd1babd08
sharon <sharon@sharonbellsimac.mynetworksettings.com>
2024-08-05 10:41:14 -0400

    sort details by type

--- src/export.py
+++ src/export.py
@@ -1,22 +1,37 @@
 from detail import Detail, DetailType
 from docx import Document
 
-def export(details: list[Detail], file: str):
+def export(details: list[Detail], file_name: str):
+    procedure_details = []
+    medication_details = []
+    allergy_details = []
+    for detail in details:
+        match detail.type:
+            case DetailType.PROCEDURE:
+                procedure_details.append(detail)
+            case DetailType.MEDICATION:
+                medication_details.append(detail)
+            case DetailType.ALLERGY:
+                allergy_details.append(detail)
+            case _:
+                print(f"unknown detail type {detail.type}")
+
+
     doc = Document()
 
     doc.add_heading("Procedures", 2)
-    for detail in details:
-        if detail.type == DetailType.PROCEDURE:
-            doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
+    for detail in procedure_details:
+        doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
 
     doc.add_heading("Medications", 2)
     # TODO: add dates
-    for detail in details:
-        if detail.type == DetailType.MEDICATION:
-            doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
+    for detail in medication_details:
+        doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
 
     doc.add_heading("Allergies", 2)
-    for detail in details:
-        if detail.type == DetailType.ALLERGY:
-            doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
+    for detail in allergy_details:
+        doc.add_paragraph(f"Name: {detail.value}\t Page: {detail.page}", "List Bullet")
+
+    doc.save(file_name)
+    # TODO check for existence
             
